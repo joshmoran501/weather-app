bb4ea592bfb116b99124d24431f8106270fd237e
josh.moran501 <josh.moran501@gmail.com>
2022-06-11 01:18:45 -0400

    some redundancies removed, also added gitignore file to store API key

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,6 @@ function displayPastCities () {
 }    
 
 
-
 function fetchWeather() {
     // get coordinates of city searched
     var city = document.getElementById(`citySearch`).value
@@ -44,47 +43,17 @@ function fetchWeather() {
                     var temperature = data.current.temp.toFixed(1)
                     var wind = data.current.wind_speed.toFixed(1)
                     var humidity = data.current.humidity
-                    var UV = data.current.uvi
+                    var UV = data.current.uvi.toFixed(2)
                     if (!cityArray.includes(cityName)) {
                         pastCities.innerHTML += `<button onclick="recallWeather(this.textContent)" type="button" class="btn btn-primary cityButton">${cityName}</button>`
                     }
                     cityArray.push(cityName)
-                    localStorage.setItem("city", JSON.stringify(cityArray))
-                    
+                    localStorage.setItem("city", JSON.stringify(cityArray));
+                   
                     // Generate current weather
-                    currentWeather.innerHTML = 
-                        `<div id="cityHeader">
-                            <h2 id="currentCity" aria-placeholder="currentCity">${cityName}</h2>
-                            <img src= "https://openweathermap.org/img/wn/`+ weatherIcon +`.png" alt="" class="icon" />
-                        </div>
-                        <p>Temp: ${temperature}&degF</p>
-                        <p>Wind: ${wind}mph</p>
-                        <p>Humidity: ${humidity}%</p>
-                        <div id="UVdiv">                
-                            <p>UV Index:</p>
-                            <p id="UVvalue">${UV}</p>
-                        </div>`
-                    // Generate the 5-day forecast
-                    for(var i=0; i<5; i++) {
-                    var dailyDate = dayjs().add(i+1,`day`).format(`MM/DD/YY`)
-                    var dailyIcon = data.daily[i].weather[0].icon
-                    var dailyMaxTemp = data.daily[i].temp.max.toFixed(0)
-                    var dailyMinTemp = data.daily[i].temp.min.toFixed(0)
-                    var dailyWind = data.daily[i].wind_speed.toFixed(0)
-                    var dailyHumidity = data.daily[i].humidity
-                    futureCards.innerHTML += `
-                    <div class="card col">
-                        <div>
-                            <h4>${dailyDate}</h4>
-                            <img src="https://openweathermap.org/img/wn/${dailyIcon}.png" alt="" class="icon" />
-                        </div>
-                        <p>Temp: ${dailyMinTemp}-${dailyMaxTemp}&degF</p>
-                        <p>Wind: ${dailyWind} mph</p>
-                        <p>Humidity: ${dailyHumidity}%</p>
-                    </div>`;
+                    displayForecast(cityName, weatherIcon, temperature, wind, humidity, UV, data);
                     UVcolor()
-                    }                    
-                }
+                }                    
             )    
         }
     )
@@ -120,8 +89,19 @@ function recallWeather(clickedText) {
                     var wind = data.current.wind_speed.toFixed(1)
                     var humidity = data.current.humidity
                     var UV = data.current.uvi
+                    
                     // Generate current weather
-                    currentWeather.innerHTML = 
+                    displayForecast(cityName, weatherIcon, temperature, wind, humidity, UV, data);
+                    UVcolor()
+                }                                    
+            )    
+        }
+    )
+}
+    
+
+function displayForecast (cityName, weatherIcon, temperature, wind, humidity, UV, data) {
+    currentWeather.innerHTML = 
                         `<div id="cityHeader">
                             <h2 id="currentCity" aria-placeholder="currentCity">${cityName}</h2>
                             <img src= "https://openweathermap.org/img/wn/`+ weatherIcon +`.png" alt="" class="icon" />
@@ -149,17 +129,11 @@ function recallWeather(clickedText) {
                             <img src="https://openweathermap.org/img/wn/${dailyIcon}.png" alt="" class="icon" />
                         </div>
                         <p>Temp: ${dailyMinTemp}-${dailyMaxTemp}&degF</p>
-                        <p>Wind: ${dailyWind}mph</p>
+                        <p>Wind: ${dailyWind} mph</p>
                         <p>Humidity: ${dailyHumidity}%</p>
                     </div>`;
-                    UVcolor()
-                    }                    
-                }
-            )    
-        }
-    )
-}
-    
+}}
+
 
 displayPastCities ()
 
@@ -177,9 +151,5 @@ function UVcolor () {
     } else UVvalue.classList.add("purple")
     }
 
-    
-
 
 searchButton.click(fetchWeather)
-
-
