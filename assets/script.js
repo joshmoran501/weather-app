77f24a9ee0731c9a055cf1af4ff0031efc984ad5
josh.moran501 <josh.moran501@gmail.com>
2022-06-11 13:18:43 -0400

    removing redundancies broke some functions, reinstated them

--- assets/script.js
+++ assets/script.js
@@ -4,32 +4,21 @@ var currentWeather = document.getElementById(`currentWeather`)
 var futureCards = document.getElementById("futureCards")
 var cityArray = JSON.parse(localStorage.getItem("city")) || []
 var pastCities = document.getElementById(`pastCities`)
+var citySearch = document.getElementById(`citySearch`)
 
 function displayPastCities () {
     cityArray.forEach(element => {
         if (pastCities.textContent.includes(element)) {
             pastCities.innerHTML += ""
         } else (pastCities.innerHTML += `<button onclick="recallWeather(this.textContent)" type="button" class="btn btn-primary">${element}</button>`)
-    })
+    });
 }    
 
-function fetchWeather() {
-    var city = document.getElementById(`citySearch`).value
-    fetchAPI (city)
-    if (!cityArray.includes(cityName)) {
-        pastCities.innerHTML += `<button onclick="recallWeather(this.textContent)" type="button" class="btn btn-primary cityButton">${cityName}</button>`
-    }
-    cityArray.push(cityName)
-    localStorage.setItem("city", JSON.stringify(cityArray))}                    
-                
 
-function recallWeather(clickedText) {
-    recallCity = clickedText
-    fetchAPI(recallCity)}
-    
-function fetchAPI (city) {
+function fetchWeather() {
+    // get coordinates of city searched
+    var city = document.getElementById(`citySearch`).value
     futureCards.innerHTML = ""
-    // get coordinates for city
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=` + APIKey +`&units=imperial`)
     .then((response) => {
         if (!response.ok) {
@@ -40,6 +29,7 @@ function fetchAPI (city) {
         })
         // use coordinates to get data from One Call
         .then((cityData) => {
+            console.log(cityData)
             var lon = cityData.coord.lon
             var lat = cityData.coord.lat
             fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`)
@@ -48,50 +38,103 @@ function fetchAPI (city) {
                 })
                 // set weather data as variables and save city to local storage
                 .then((data) => {
+                    console.log(data)
                     var cityName = cityData.name
                     var weatherIcon = data.current.weather[0].icon
                     var temperature = data.current.temp.toFixed(1)
                     var wind = data.current.wind_speed.toFixed(1)
                     var humidity = data.current.humidity
                     var UV = data.current.uvi.toFixed(2)
+                    if (!cityArray.includes(cityName)) {
+                        pastCities.innerHTML += `<button onclick="recallWeather(this.textContent)" type="button" class="btn btn-primary cityButton">${cityName}</button>`
+                    }
+                    cityArray.push(cityName)
+                    localStorage.setItem("city", JSON.stringify(cityArray));
+                   
+                    // Generate current weather
                     displayForecast(cityName, weatherIcon, temperature, wind, humidity, UV, data);
                     UVcolor()
-                })})}
+                    citySearch.value = ""
+                }                    
+            )    
+        }
+    )
+}
 
+function recallWeather(clickedText) {
+    // get coordinates of previously searched city
+    recallCity = clickedText
+    futureCards.innerHTML = ""
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=` + recallCity + `&appid=` + APIKey +`&units=imperial`)
+    .then((response) => {
+        if (!response.ok) {
+            alert("No city found.");
+            throw new Error("No city found.");
+        }
+        return response.json();
+        })
+        // use coordinates to get data from One Call
+        .then((cityData) => {
+            console.log(cityData)
+            var lon = cityData.coord.lon
+            var lat = cityData.coord.lat
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`)
+            .then((response) => {
+                return response.json();
+                })
+                // set weather data as variables and save city to local storage
+                .then((data) => {
+                    console.log(data)
+                    var cityName = cityData.name
+                    var weatherIcon = data.current.weather[0].icon
+                    var temperature = data.current.temp.toFixed(1)
+                    var wind = data.current.wind_speed.toFixed(1)
+                    var humidity = data.current.humidity
+                    var UV = data.current.uvi.toFixed(2)
+                    
+                    // Generate current weather and 5 day forecast
+                    displayForecast(cityName, weatherIcon, temperature, wind, humidity, UV, data);
+                    UVcolor()
+                    citySearch.value = ""
+                }                                    
+            )    
+        }
+    )
+}
+    
 
 function displayForecast (cityName, weatherIcon, temperature, wind, humidity, UV, data) {
-    // generate current weather
     currentWeather.innerHTML = 
-        `<div id="cityHeader">
-            <h2 id="currentCity" aria-placeholder="currentCity">${cityName}</h2>
-            <img src= "https://openweathermap.org/img/wn/`+ weatherIcon +`.png" alt="" class="icon" />
-        </div>
-        <p>Temp: ${temperature}&degF</p>
-        <p>Wind: ${wind} mph</p>
-        <p>Humidity: ${humidity}%</p>
-        <div id="UVdiv">                
-            <p>UV Index:</p>
-            <p id="UVvalue" name="${UV}">${UV}</p>
-        </div>`
-
-    // Generate the 5-day forecast
-    for(var i=0; i<5; i++) {
-    var dailyDate = dayjs().add(i+1,`day`).format(`MM/DD/YY`)
-    var dailyIcon = data.daily[i].weather[0].icon
-    var dailyMaxTemp = data.daily[i].temp.max.toFixed(0)
-    var dailyMinTemp = data.daily[i].temp.min.toFixed(0)
-    var dailyWind = data.daily[i].wind_speed.toFixed(0)
-    var dailyHumidity = data.daily[i].humidity
-    futureCards.innerHTML += `
-    <div class="card col">
-        <div>
-            <h4>${dailyDate}</h4>
-            <img src="https://openweathermap.org/img/wn/${dailyIcon}.png" alt="" class="icon" />
-        </div>
-        <p>Temp: ${dailyMinTemp}-${dailyMaxTemp}&degF</p>
-        <p>Wind: ${dailyWind} mph</p>
-        <p>Humidity: ${dailyHumidity}%</p>
-    </div>`}}
+    `<div id="cityHeader">
+        <h2 id="currentCity" aria-placeholder="currentCity">${cityName}</h2>
+        <img src= "https://openweathermap.org/img/wn/`+ weatherIcon +`.png" alt="" class="icon" />
+    </div>
+    <p>Temp: ${temperature}&degF</p>
+    <p>Wind: ${wind} mph</p>
+    <p>Humidity: ${humidity}%</p>
+    <div id="UVdiv">                
+        <p>UV Index:</p>
+        <p id="UVvalue" name="${UV}">${UV}</p>
+    </div>`
+                    
+        // Generate the 5-day forecast
+        for(var i=0; i<5; i++) {
+        var dailyDate = dayjs().add(i+1,`day`).format(`MM/DD/YY`)
+        var dailyIcon = data.daily[i].weather[0].icon
+        var dailyMaxTemp = data.daily[i].temp.max.toFixed(0)
+        var dailyMinTemp = data.daily[i].temp.min.toFixed(0)
+        var dailyWind = data.daily[i].wind_speed.toFixed(0)
+        var dailyHumidity = data.daily[i].humidity
+        futureCards.innerHTML += `
+        <div class="card col">
+            <div>
+                <h4>${dailyDate}</h4>
+                <img src="https://openweathermap.org/img/wn/${dailyIcon}.png" alt="" class="icon" />
+            </div>
+            <p>Temp: ${dailyMinTemp}-${dailyMaxTemp}&degF</p>
+            <p>Wind: ${dailyWind} mph</p>
+            <p>Humidity: ${dailyHumidity}%</p>
+        </div>`;}}
 
 displayPastCities ()
 
@@ -108,5 +151,6 @@ function UVcolor () {
         UVvalue.classList.add("red")
     } else UVvalue.classList.add("purple")
     }
+
 
 searchButton.click(fetchWeather)
