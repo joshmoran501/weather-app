b878a8d64999d6a1520e980101e26c86cd99928c
josh.moran501 <josh.moran501@gmail.com>
2026-05-28 15:04:34 -0500

    Updated api call to work with new api, removed UV index as it is not a part of the free api call now

--- assets/script.js
+++ assets/script.js
@@ -27,36 +27,34 @@ function fetchWeather() {
         }
         return response.json();
         })
-        // use coordinates to get data from One Call
+        // use coordinates to get the 5-day forecast
         .then((cityData) => {
             console.log(cityData)
             var lon = cityData.coord.lon
             var lat = cityData.coord.lat
-            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`)
+            // current weather comes from this first call
+            var cityName = cityData.name
+            var weatherIcon = cityData.weather[0].icon
+            var temperature = cityData.main.temp.toFixed(1)
+            var wind = cityData.wind.speed.toFixed(1)
+            var humidity = cityData.main.humidity
+            if (!cityArray.includes(cityName)) {
+                pastCities.innerHTML += `<button onclick="recallWeather(this.textContent)" type="button" class="btn btn-primary cityButton">${cityName}</button>`
+            }
+            cityArray.push(cityName)
+            localStorage.setItem("city", JSON.stringify(cityArray));
+
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`)
             .then((response) => {
                 return response.json();
                 })
-                // set weather data as variables and save city to local storage
                 .then((data) => {
                     console.log(data)
-                    var cityName = cityData.name
-                    var weatherIcon = data.current.weather[0].icon
-                    var temperature = data.current.temp.toFixed(1)
-                    var wind = data.current.wind_speed.toFixed(1)
-                    var humidity = data.current.humidity
-                    var UV = data.current.uvi.toFixed(2)
-                    if (!cityArray.includes(cityName)) {
-                        pastCities.innerHTML += `<button onclick="recallWeather(this.textContent)" type="button" class="btn btn-primary cityButton">${cityName}</button>`
-                    }
-                    cityArray.push(cityName)
-                    localStorage.setItem("city", JSON.stringify(cityArray));
-                   
-                    // Generate current weather
-                    displayForecast(cityName, weatherIcon, temperature, wind, humidity, UV, data);
-                    UVcolor()
+                    // Generate current weather and 5 day forecast
+                    displayForecast(cityName, weatherIcon, temperature, wind, humidity, data);
                     citySearch.value = ""
-                }                    
-            )    
+                }
+            )
         }
     )
 }
@@ -73,37 +71,35 @@ function recallWeather(clickedText) {
         }
         return response.json();
         })
-        // use coordinates to get data from One Call
+        // use coordinates to get the 5-day forecast
         .then((cityData) => {
             console.log(cityData)
             var lon = cityData.coord.lon
             var lat = cityData.coord.lat
-            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`)
+            // current weather comes from this first call
+            var cityName = cityData.name
+            var weatherIcon = cityData.weather[0].icon
+            var temperature = cityData.main.temp.toFixed(1)
+            var wind = cityData.wind.speed.toFixed(1)
+            var humidity = cityData.main.humidity
+
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`)
             .then((response) => {
                 return response.json();
                 })
-                // set weather data as variables and save city to local storage
                 .then((data) => {
                     console.log(data)
-                    var cityName = cityData.name
-                    var weatherIcon = data.current.weather[0].icon
-                    var temperature = data.current.temp.toFixed(1)
-                    var wind = data.current.wind_speed.toFixed(1)
-                    var humidity = data.current.humidity
-                    var UV = data.current.uvi.toFixed(2)
-                    
                     // Generate current weather and 5 day forecast
-                    displayForecast(cityName, weatherIcon, temperature, wind, humidity, UV, data);
-                    UVcolor()
+                    displayForecast(cityName, weatherIcon, temperature, wind, humidity, data);
                     citySearch.value = ""
-                }                                    
-            )    
+                }
+            )
         }
     )
 }
     
 
-function displayForecast (cityName, weatherIcon, temperature, wind, humidity, UV, data) {
+function displayForecast (cityName, weatherIcon, temperature, wind, humidity, data) {
     currentWeather.innerHTML = 
     `<div id="cityHeader">
         <h2 id="currentCity" aria-placeholder="currentCity">${cityName}</h2>
@@ -111,46 +107,54 @@ function displayForecast (cityName, weatherIcon, temperature, wind, humidity, UV
     </div>
     <p>Temp: ${temperature}&degF</p>
     <p>Wind: ${wind} mph</p>
-    <p>Humidity: ${humidity}%</p>
-    <div id="UVdiv">                
-        <p>UV Index:</p>
-        <p id="UVvalue" name="${UV}">${UV}</p>
-    </div>`
+    <p>Humidity: ${humidity}%</p>`
                     
+        // The free forecast endpoint returns 3-hour intervals in data.list.
+        // Group them by calendar day so we can build one card per day.
+        var dailyData = {}
+        data.list.forEach(function (entry) {
+            var day = entry.dt_txt.split(" ")[0] // "YYYY-MM-DD"
+            if (!dailyData[day]) {
+                dailyData[day] = []
+            }
+            dailyData[day].push(entry)
+        })
+
+        // Skip today (already shown as current weather), take the next 5 days
+        var today = dayjs().format(`YYYY-MM-DD`)
+        var forecastDays = Object.keys(dailyData).filter(function (d) {
+            return d !== today
+        }).slice(0, 5)
+
         // Generate the 5-day forecast
-        for(var i=0; i<5; i++) {
-        var dailyDate = dayjs().add(i+1,`day`).format(`MM/DD/YY`)
-        var dailyIcon = data.daily[i].weather[0].icon
-        var dailyMaxTemp = data.daily[i].temp.max.toFixed(0)
-        var dailyMinTemp = data.daily[i].temp.min.toFixed(0)
-        var dailyWind = data.daily[i].wind_speed.toFixed(0)
-        var dailyHumidity = data.daily[i].humidity
-        futureCards.innerHTML += `
-        <div class="card col">
-            <div>
-                <h4>${dailyDate}</h4>
-                <img src="https://openweathermap.org/img/wn/${dailyIcon}.png" alt="" class="icon" />
-            </div>
-            <p>Temp: ${dailyMinTemp}-${dailyMaxTemp}&degF</p>
-            <p>Wind: ${dailyWind} mph</p>
-            <p>Humidity: ${dailyHumidity}%</p>
-        </div>`;}}
+        for (var i = 0; i < forecastDays.length; i++) {
+            var entries = dailyData[forecastDays[i]]
+            var temps = entries.map(function (e) { return e.main.temp })
+            // use the midday reading (or middle entry) as the day's snapshot
+            var midday = entries.find(function (e) {
+                return e.dt_txt.includes("12:00:00")
+            }) || entries[Math.floor(entries.length / 2)]
+
+            var dailyDate = dayjs(forecastDays[i]).format(`MM/DD/YY`)
+            var dailyIcon = midday.weather[0].icon
+            var dailyMaxTemp = Math.max.apply(null, temps).toFixed(0)
+            var dailyMinTemp = Math.min.apply(null, temps).toFixed(0)
+            var dailyWind = midday.wind.speed.toFixed(0)
+            var dailyHumidity = midday.main.humidity
+            futureCards.innerHTML += `
+            <div class="card col">
+                <div>
+                    <h4>${dailyDate}</h4>
+                    <img src="https://openweathermap.org/img/wn/${dailyIcon}.png" alt="" class="icon" />
+                </div>
+                <p>Temp: ${dailyMinTemp}-${dailyMaxTemp}&degF</p>
+                <p>Wind: ${dailyWind} mph</p>
+                <p>Humidity: ${dailyHumidity}%</p>
+            </div>`
+        }
+    }
 
 displayPastCities ()
-
-function UVcolor () {
-    var UVvalue = document.getElementById("UVvalue")
-    var displayedUV = parseInt(UVvalue.getAttribute("name"))
-    if(displayedUV<=2) {
-        UVvalue.classList.add("green")
-    } else if (displayedUV>2 && displayedUV<6) {
-        UVvalue.classList.add("yellow")
-    } else if (displayedUV>=6 && displayedUV<8){
-        UVvalue.classList.add("orange")
-    } else if (displayedUV>=8 && displayedUV<11){
-        UVvalue.classList.add("red")
-    } else UVvalue.classList.add("purple")
-    }
 
 
 searchButton.click(fetchWeather)
